4febdd247d150097c601574e3c4d1a49ac13e5b0
mahsa reshadi <mahsa.reshadi@gmail.com>
2021-09-30 11:38:59 -0700

    Updates

--- src/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import logo from './Logo.png';
-import './Logo.css'
-function Logo() {
-    return (
-        <div id="img">
-            <img id="makeup-image" src={logo} alt="my-pic"/>
-        </div>);
-}
-export default Logo;
